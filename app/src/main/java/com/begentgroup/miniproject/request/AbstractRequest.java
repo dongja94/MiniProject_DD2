d66be4ea6d1445f4478ae6459270b7c32c7fd2d5
Yon Seungik <dongja94@gmail.com>
2016-08-09 17:18:32 +0900

    AbstractRequest update

--- app/src/main/java/com/begentgroup/miniproject/request/AbstractRequest.java
+++ app/src/main/java/com/begentgroup/miniproject/request/AbstractRequest.java
@@ -1,8 +1,16 @@
 package com.begentgroup.miniproject.request;
 
+import com.begentgroup.miniproject.data.NetworkResult;
+import com.begentgroup.miniproject.data.NetworkResultTemp;
 import com.begentgroup.miniproject.manager.NetworkRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
 
 import okhttp3.HttpUrl;
+import okhttp3.ResponseBody;
 
 /**
  * Created by Administrator on 2016-08-09.
@@ -15,4 +23,21 @@ public abstract class AbstractRequest<T> extends NetworkRequest<T> {
         builder.host("dongjatestweb.appspot.com");
         return builder;
     }
+
+    @Override
+    protected T parse(ResponseBody body) throws IOException {
+        String text = body.string();
+        Gson gson = new Gson();
+        NetworkResultTemp temp = gson.fromJson(text, NetworkResultTemp.class);
+        if (temp.getCode() == 1) {
+            T result = gson.fromJson(text, getType());
+            return result;
+        } else {
+            Type type = new TypeToken<NetworkResult<String>>(){}.getType();
+            NetworkResult<String> result = gson.fromJson(text, type);
+            throw new IOException(result.getResult());
+        }
+    }
+
+    protected abstract Type getType();
 }
--- app/src/main/java/com/begentgroup/miniproject/request/FriendListRequest.java
+++ app/src/main/java/com/begentgroup/miniproject/request/FriendListRequest.java
@@ -3,18 +3,14 @@ package com.begentgroup.miniproject.request;
 import android.content.Context;
 
 import com.begentgroup.miniproject.data.NetworkResult;
-import com.begentgroup.miniproject.data.NetworkResultTemp;
 import com.begentgroup.miniproject.data.User;
-import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
 
 import okhttp3.HttpUrl;
 import okhttp3.Request;
-import okhttp3.ResponseBody;
 
 /**
  * Created by Administrator on 2016-08-09.
@@ -35,19 +31,24 @@ public class FriendListRequest extends AbstractRequest<NetworkResult<List<User>>
         return request;
     }
 
+//    @Override
+//    protected NetworkResult<List<User>> parse(ResponseBody body) throws IOException {
+//        String text = body.string();
+//        Gson gson = new Gson();
+//        NetworkResultTemp temp = gson.fromJson(text, NetworkResultTemp.class);
+//        if (temp.getCode() == 1) {
+//            Type type = new TypeToken<NetworkResult<List<User>>>(){}.getType();
+//            NetworkResult<List<User>> result = gson.fromJson(text, type);
+//            return result;
+//        } else {
+//            Type type = new TypeToken<NetworkResult<String>>(){}.getType();
+//            NetworkResult<String> result = gson.fromJson(text, type);
+//            throw new IOException(result.getResult());
+//        }
+//    }
+
     @Override
-    protected NetworkResult<List<User>> parse(ResponseBody body) throws IOException {
-        String text = body.string();
-        Gson gson = new Gson();
-        NetworkResultTemp temp = gson.fromJson(text, NetworkResultTemp.class);
-        if (temp.getCode() == 1) {
-            Type type = new TypeToken<NetworkResult<List<User>>>(){}.getType();
-            NetworkResult<List<User>> result = gson.fromJson(text, type);
-            return result;
-        } else {
-            Type type = new TypeToken<NetworkResult<String>>(){}.getType();
-            NetworkResult<String> result = gson.fromJson(text, type);
-            throw new IOException(result.getResult());
-        }
+    protected Type getType() {
+        return new TypeToken<NetworkResult<List<User>>>(){}.getType();
     }
 }
