718a0f6cd331a23381b50be553809cd9ed5af529
Yon Seungik <dongja94@gmail.com>
2016-08-11 14:43:55 +0900

    chat

--- app/src/main/java/com/begentgroup/miniproject/MainActivity.java
+++ app/src/main/java/com/begentgroup/miniproject/MainActivity.java
@@ -2,54 +2,34 @@ package com.begentgroup.miniproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.begentgroup.miniproject.data.NetworkResult;
-import com.begentgroup.miniproject.data.User;
 import com.begentgroup.miniproject.login.SimpleLoginActivity;
 import com.begentgroup.miniproject.manager.NetworkManager;
 import com.begentgroup.miniproject.manager.NetworkRequest;
 import com.begentgroup.miniproject.manager.PropertyManager;
-import com.begentgroup.miniproject.request.FriendListRequest;
 import com.begentgroup.miniproject.request.LogOutRequest;
-
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.listView)
-    ListView listView;
+    @BindView(R.id.tabhost)
+    FragmentTabHost tabHost;
 
-    ArrayAdapter<User> mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        mAdapter = new ArrayAdapter<User>(this, android.R.layout.simple_list_item_1);
-        listView.setAdapter(mAdapter);
-
-        FriendListRequest request = new FriendListRequest(this);
-        NetworkManager.getInstance().getNetworkData(request, new NetworkManager.OnResultListener<NetworkResult<List<User>>>() {
-            @Override
-            public void onSuccess(NetworkRequest<NetworkResult<List<User>>> request, NetworkResult<List<User>> result) {
-                List<User> users = result.getResult();
-                mAdapter.addAll(users);
-            }
-
-            @Override
-            public void onFail(NetworkRequest<NetworkResult<List<User>>> request, int errorCode, String errorMessage, Throwable e) {
-
-            }
-        });
+        tabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+        tabHost.addTab(tabHost.newTabSpec("main").setIndicator("Main"), MainFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("chat").setIndicator("Chat"), ChatUserFragment.class, null);
     }
 
     @Override
--- app/src/main/java/com/begentgroup/miniproject/data/User.java
+++ app/src/main/java/com/begentgroup/miniproject/data/User.java
@@ -1,6 +1,8 @@
 package com.begentgroup.miniproject.data;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private long id;
     private String userName;
     private String email;
