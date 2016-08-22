c07194879e69768ccd52427272cec0286d8f693a
Yon Seungik <dongja94@gmail.com>
2016-08-22 14:55:30 +0900

    gcm

--- app/src/main/java/com/begentgroup/miniproject/gcm/MyGcmListenerService.java
+++ app/src/main/java/com/begentgroup/miniproject/gcm/MyGcmListenerService.java
@@ -85,6 +85,7 @@ public class MyGcmListenerService extends GcmListenerService {
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker("GCM Message")
                 .setContentTitle("GCM Message")
                 .setContentText(message)
                 .setAutoCancel(true)
