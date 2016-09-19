c870b7b6564f06932d4e2c1d3ef80c1375e13058
Yon Seungik <dongja94@gmail.com>
2016-09-19 11:02:53 +0900

    cookie

--- app/src/main/java/com/begentgroup/miniproject/manager/NetworkManager.java
+++ app/src/main/java/com/begentgroup/miniproject/manager/NetworkManager.java
@@ -6,6 +6,7 @@ import android.os.Looper;
 import android.os.Message;
 
 import com.begentgroup.miniproject.MyApplication;
+import com.begentgroup.miniproject.R;
 import com.franmontiel.persistentcookiejar.ClearableCookieJar;
 import com.franmontiel.persistentcookiejar.PersistentCookieJar;
 import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
@@ -13,12 +14,36 @@ import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersisto
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.Cache;
 import okhttp3.Call;
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
 import okhttp3.Dispatcher;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 
 /**
@@ -58,6 +83,82 @@ public class NetworkManager {
 
         client = builder.build();
     }
+
+    public Map<String,String> getCookieHeader(String url) throws MalformedURLException {
+        Map<String,String> headers = new HashMap<>();
+        CookieJar cookieJar = client.cookieJar();
+        HttpUrl httpUrl = HttpUrl.get(new URL(url));
+        List<Cookie>  cookies = cookieJar.loadForRequest(httpUrl);
+        StringBuilder sb = new StringBuilder();
+        int size = cookies.size();
+        for (int i = 0 ; i < cookies.size(); i++) {
+            Cookie cookie = cookies.get(i);
+            if (i > 0) {
+                sb.append(";");
+            }
+            sb.append(cookie.name()).append("=").append(cookie.value());
+        }
+        headers.put("Cookie",sb.toString());
+        return headers;
+    }
+
+    void disableCertificateValidation(Context context, OkHttpClient.Builder builder) {
+
+        try {
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            InputStream caInput = context.getResources().openRawResource(R.raw.ca);
+            Certificate ca;
+            try {
+                ca = cf.generateCertificate(caInput);
+                System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN());
+            } finally {
+                caInput.close();
+            }
+            String keyStoreType = KeyStore.getDefaultType();
+            KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+            keyStore.load(null, null);
+            keyStore.setCertificateEntry("ca", ca);
+
+            KeyStore defaultStore = KeyStore.getInstance("AndroidCAStore");
+            defaultStore.load(null, null);
+            for (Enumeration<String> en = defaultStore.aliases(); en.hasMoreElements();) {
+                final String alias = en.nextElement();
+                final X509Certificate cert = (X509Certificate) defaultStore.getCertificate(alias);
+                if (cert != null) {
+                    keyStore.setCertificateEntry(alias, cert);
+                }
+            }
+
+            String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+            tmf.init(keyStore);
+
+            X509TrustManager tm = (X509TrustManager)tmf.getTrustManagers()[0];
+
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, tmf.getTrustManagers(), null);
+            HostnameVerifier hv = new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+//            sc.init(null, tmf.getTrustManagers(), null);
+            sc.init(null, new TrustManager[]{tm}, null);
+            builder.sslSocketFactory(sc.getSocketFactory(), tm);
+            builder.hostnameVerifier(hv);
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public OkHttpClient getClient() {
         return client;
